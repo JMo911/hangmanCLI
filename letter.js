75dec7885e14fc0a69c66365f75c4cbb9ee44ce5
Jacob Moss <jacobmoss09@gmail.com>
2019-08-06 19:28:48 -0500

    word stores and splits into each character. structure of constructor is laid out.

--- letter.js
+++ letter.js
@@ -6,4 +6,22 @@
 //   .then(answers => {
 //     // Use user feedback for... whatever!!
 //   });
+var word = "Jacob";
 
+function Letter(str, alreadyGuessed) {
+    this.str = str;
+    this.alreadyGuessed = alreadyGuessed;
+    this.placeholder = function() {
+
+    };
+    this.characterCheck = function() {
+
+    };
+
+};
+
+var wordArray = word.split("");
+
+wordArray.forEach(function(e){
+    console.log(e);
+});
