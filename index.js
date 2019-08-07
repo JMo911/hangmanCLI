8bdeaaf92eb17382397da6b8696f6d73c5e0a8ed
Jacob Moss <jacobmoss09@gmail.com>
2019-08-06 19:40:57 -0500

    pseudocode logic for game play is laid out. Word constructor requirement input on line 1

---
+++ index.js
@@ -0,0 +1,11 @@
+var Word = require("./word");
+
+
+
+function runGame() {
+    //need to randomly select a word from my array
+    // var random = Math.floor(Math.random(wordarraylength) +1);
+    //use the Word constructor to store my chosen word
+    //prompt user for guesses, and decrement remaining guesses
+    //handle win/loss messaging and logic.
+}
