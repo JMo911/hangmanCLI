552c830506d8162be8edd8b214c8a3f25642575d
Jacob Moss <jacobmoss09@gmail.com>
2019-08-06 22:00:14 -0500

    closing out for 8.6.19. First inquirer prompt is in and responsive. Rest of game function needs to be built out.

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 var Word = require("./word");
-
-
+var inquirer = require('inquirer');
 
 function runGame() {
+    var possibleWords = [
+        "AUSTRIA", 
+        "COSTA RICA", 
+        "EGYPT", 
+        "INDIA", 
+        "SPAIN",
+        "SWITZERLAND",
+        "UNITED STATES OF AMERICA"
+    ];
+    //select random word from my array and create a new word object for it
+    var randomWord='';
+    randomWord = possibleWords[Math.floor(Math.random()*(possibleWords.length))];
+    new Word(randomWord);
+    //begin prompting user for guesses
+    inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: "input",
+            name: "guess",
+            message: "Please guess a letter."
+        }
+    ])
+    .then(answers => {
+        // Use user feedback for... whatever!!
+        // console.log("users guess: " + answers.guess);
+    });
+
+
     //need to randomly select a word from my array
     // var random = Math.floor(Math.random(wordarraylength) +1);
     //use the Word constructor to store my chosen word
     //prompt user for guesses, and decrement remaining guesses
     //handle win/loss messaging and logic.
-}
+};
+runGame();
--- letter.js
+++ letter.js
@@ -1,12 +1,3 @@
-// var inquirer = require('inquirer');
-// inquirer
-//   .prompt([
-//     /* Pass your questions in here */
-//   ])
-//   .then(answers => {
-//     // Use user feedback for... whatever!!
-//   });
-
 function Letter(str, alreadyGuessed) {
     this.str = str;
     this.alreadyGuessed = alreadyGuessed;
