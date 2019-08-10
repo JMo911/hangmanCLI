4f1324c3c031025b815cb2bdf14dcaaf39b74e7c
Jacob Moss <jacobmoss09@gmail.com>
2019-08-09 22:58:54 -0500

    init function split out to avoid win loss scenario bugs

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
 var Word = require("./word");
 var inquirer = require('inquirer');
 
-function runGame() {
-    var guessesRemaining;
-    var possibleWords = [
-        "AUSTRIA", 
-        "COSTA RICA", 
-        "EGYPT", 
-        "INDIA", 
-        "SPAIN",
-        "SWITZERLAND",
-        "UNITED STATES OF AMERICA"
-    ];
-    //select random word from my array and create a new word object for it
-    var randomWord='';
-    randomWord = possibleWords[Math.floor(Math.random()*(possibleWords.length))];
-    var constructedWord = new Word(randomWord);
-    //turn the word into my array of objects first
-    var wordArray = constructedWord.newArray();
-    guessesRemaining = wordArray.length*1.2;
-    
-    //guessesRemaining=constructedWord.length*1.4;
+var wins=0;
+var losses=0;
+var guessesRemaining;
 
-    //call the string representation method on that array of objects to display the word as expected.
-    
-    console.log(randomWord);
+var possibleWords = [
+    "AUSTRIA", 
+    "EGYPT", 
+    "INDIA", 
+    "SPAIN",
+    "SWITZERLAND"
+];
+var randomWord;
+var wordArray;
+var constructedWord 
+
+if (wins===0 && losses ===0) {
+    console.log("Welcome to hangman! Type a letter and press enter to begin guessing.")
+};
+
+function init() {
+    randomWord = possibleWords[Math.floor(Math.random()*(possibleWords.length))];
+    constructedWord = new Word(randomWord);
+    wordArray = constructedWord.newArray();
+    guessesRemaining = Math.floor(wordArray.length*1.2);
+    // console.log(wordArray);
+}
+init();
+
+
+
+
+function runGame() {
     console.log(constructedWord.stringRep());
     //begin prompting user for guesses
-    // console.log("Welcome to hangman! Type a letter and press enter to begin guessing.");
 
     guessing();
 
     function guessing(){
-    inquirer
-    .prompt([
-        /* Pass your questions in here */
-        {
-            type: "input",
-            name: "guess",
-            message: "Please guess a letter."
-        }
-    ])
-    .then(answers => {
-        // Use user feedback for... whatever!!
-        // console.log("users guess: " + answers.guess);
-        if (guessesRemaining>0) {
-        constructedWord.guess(answers.guess.toUpperCase());
-        console.log(constructedWord.stringRep());
-        console.log("Guesses Remaining: " + guessesRemaining--);
-        guessing();} else {
-            runGame();
-        }
-    });
+        inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: "input",
+                name: "guess",
+                message: "Please guess a letter."
+            }
+        ])
+        .then(answers => {
+            if (guessesRemaining>=0 && constructedWord.stringRep().indexOf("_") != -1) {
+                constructedWord.guess(answers.guess.toUpperCase());
+                console.log(constructedWord.stringRep());
+                console.log("Guesses Remaining: " + guessesRemaining--);
+                guessing();
+            } else if(guessesRemaining>0 && constructedWord.stringRep().indexOf("_") === -1) {
+                init();
+                console.log("Congrats!! You won. Try another word.\n")
+                wins++;
+                console.log("Wins: " + wins + "\nLosses: " + losses);
+                
+                runGame();
+            } else {
+                init();
+                console.log("Sorry, you lost this round. Try another word.\n");
+                losses++;
+                console.log("Wins: " + wins + "\nLosses: " + losses);
+                
+                runGame();
+            }
+        });
     };
 
 
